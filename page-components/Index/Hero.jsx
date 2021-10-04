18a045a2139e570dbce0eb54db0ed87a38d846e6
tsmoreau <64439028+tsmoreau@users.noreply.github.com>
2021-10-04 11:17:45 -0700

    Initial commit

---
+++ page-components/Index/Hero.jsx
@@ -0,0 +1,40 @@
+import { ButtonLink } from '@/components/Button';
+import { Container, Spacer, Wrapper } from '@/components/Layout';
+import Link from 'next/link';
+import styles from './Hero.module.css';
+
+const Hero = () => {
+  return (
+    <Wrapper>
+      <div>
+        <h1 className={styles.title}>
+          <span className={styles.nextjs}>illoMX</span>
+          <span className={styles.mongodb}>nft market</span>
+        </h1>
+        <Container justifyContent="center" className={styles.buttons}>
+          <Container>
+            <Link passHref href="/feed">
+              <ButtonLink className={styles.button}>Explore Feed</ButtonLink>
+            </Link>
+          </Container>
+          <Spacer axis="horizontal" size={1} />
+          <Container>
+            <ButtonLink
+              href="https://github.com/hoangvvo/nextjs-mongodb-app"
+              type="secondary"
+              className={styles.button}
+            >
+              GitHub
+            </ButtonLink>
+          </Container>
+        </Container>
+        <p className={styles.subtitle}>
+          A Next.js and MongoDB web application, designed with simplicity for
+          learning and real-world applicability in mind.
+        </p>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Hero;
